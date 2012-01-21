d1efff59db5d84140705bd7dee3599f0f07dd273
ninjascience <ninjascience@gmail.com>
2012-01-21 15:16:09 -0800

    added image proxy so I could sample image pixels via canvas

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , url = require('url')
+  , http = require('http');
 
 var app = module.exports = express.createServer();
 
@@ -27,6 +29,37 @@ app.configure('development', function(){
 app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
+
+
+// image proxy
+
+app.get('/flickr_image/:image_url', function(request_from_client, response_to_client){
+  //sys.puts("Starting proxy");
+  var image_url = request_from_client.params.image_url;
+
+  var image_host_name = url.parse(image_url).hostname
+  var filename = url.parse(image_url).pathname.split("/").pop()
+
+  var http_client = http.createClient(80, image_host_name);
+  var image_get_request = http_client.request('GET', image_url, {"host": image_host_name});
+  image_get_request.addListener('response', function(proxy_response){
+    var current_byte_index = 0;
+    var response_content_length = parseInt(proxy_response.header("Content-Length"));
+    var response_body = new Buffer(response_content_length);
+   
+    proxy_response.setEncoding('binary');
+    proxy_response.addListener('data', function(chunk){
+      response_body.write(chunk, current_byte_index, "binary");
+      current_byte_index += chunk.length;
+    });
+    proxy_response.addListener('end', function(){
+      response_to_client.contentType(filename);
+      response_to_client.send(response_body);
+    });
+  });
+  image_get_request.end();
+});
+
 
 // Routes
 
